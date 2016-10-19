01db25b260bcb8306a0313b0387e4860a3c64e08
lzlzlz911 <lzlzlz911@hotmail.com>
2016-10-19 17:58:01 +0800

    Add project files.

---
+++ MasterApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MasterApp
+{
+    using Apache.NMS;
+    using Apache.NMS.ActiveMQ;
+    using Apache.NMS.ActiveMQ.Commands;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IConnectionFactory factory = new ConnectionFactory("tcp://192.168.99.100:61616");
+
+            using (IConnection connection = factory.CreateConnection())
+            {
+                using (ISession session = connection.CreateSession())
+                {
+                    IMessageProducer prod = session.CreateProducer(new ActiveMQQueue("firstqueue"));
+
+                    ITextMessage message = prod.CreateTextMessage();
+
+                    message.Text = "hw";
+
+                    prod.Send(message);
+                }
+            }
+        }
+    }
+}
---
+++ SlaveApp/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlaveApp
+{
+    using Apache.NMS;
+    using Apache.NMS.ActiveMQ;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IConnectionFactory factory = new ConnectionFactory("tcp://192.168.99.100:61616");
+
+            IConnection connection = factory.CreateConnection();
+
+            connection.ClientId = "firstQueueListener";
+
+            connection.Start();
+
+            ISession session = connection.CreateSession();
+
+            //            IMessageConsumer consumer = session.CreateConsumer(new Apache.NMS.ActiveMQ.Commands.ActiveMQQueue("firstqueue"), "filter='demo'");
+            IMessageConsumer consumer = session.CreateConsumer(new Apache.NMS.ActiveMQ.Commands.ActiveMQQueue("firstqueue"));
+
+            consumer.Listener += new MessageListener(consumer_Listener);
+
+            Console.Read();
+        }
+
+        static void consumer_Listener(IMessage message)
+        {
+            ITextMessage msg = (ITextMessage)message;
+
+        }
+    }
+}
